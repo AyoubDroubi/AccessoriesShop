5c88858a21beb83f51f039a33edc31425d5ab0fc
Ayoub Droubi <ayoub.al.droubi@gmail.com>
2025-08-26 19:33:51 +0300

    edit

--- AccessoriesShop/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AccessoriesShop.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
